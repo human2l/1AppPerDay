ef8dfe8ea1c7879f98b14556d02caf4064ec6b56
Yiming <543614772@qq.com>
2018-04-08 17:33:28 +1000

    Changed EditorService

--- UTS.ScheduleSystem/src/UTS.ScheduleSystem/EditorService.cs
+++ UTS.ScheduleSystem/src/UTS.ScheduleSystem/EditorService.cs
@@ -7,5 +7,84 @@ namespace UTS.ScheduleSystem
 {
     public class EditorService
     {
+        
+        public List<FixedConversationalRule> AddNewFCRule(FixedConversationalRule rule, List<FixedConversationalRule> fCRulesList)
+        {
+            return fCRulesList;
+        }
+
+        public List<ConversationalRule> AddNewCRule(ConversationalRule rule, List<ConversationalRule> cRulesList)
+        {
+            return cRulesList;
+        }
+
+        public List<FixedConversationalRule> ShowAllPendingFCRules(List<FixedConversationalRule> fCRulesList)
+        {
+            
+            return fCRulesList;
+        }
+
+        public List<ConversationalRule> ShowAllPendingCRules(List<ConversationalRule> cRulesList)
+        {
+            return cRulesList;
+        }
+
+        public List<FixedConversationalRule> ShowAllRejectewdFCRules(List<FixedConversationalRule> fCRulesList)
+        {
+            return fCRulesList;
+        }
+
+        public List<ConversationalRule> ShowAllRejectedCRules(List<ConversationalRule> cRulesList)
+        {
+            return cRulesList;
+        }
+
+        public void EditPendingFCRule(FixedConversationalRule fCRule)
+        {
+
+        }
+
+        public void EditPendingCRule(ConversationalRule cRule)
+        {
+
+        }
+
+        public void DeletePendingFCRule(FixedConversationalRule fCRule)
+        {
+
+        }
+
+        public void DeletePendingCRule(ConversationalRule cRule)
+        {
+
+        }
+
+        public List<FixedConversationalRule> ShowCurrentUserApprovedFCRules(User user)
+        {
+            return null;
+        }
+
+        public List<ConversationalRule> ShowCurrentUserApprovedCRules(User user)
+        {
+            return null;
+        }
+
+        public int ShowCurrentUserApprovedRulesCount(User user)
+        {
+            int count = 0;
+            return count;
+        }
+
+        public int ShowCurrentUserRejectedRulesCount(User user)
+        {
+            int count = 0;
+            return count;
+        }
+
+        public double ShowCurrentUserSuccessRate(User user)
+        {
+            double rate = 0;
+            return rate;
+        }
     }
 }
--- UTS.ScheduleSystem/src/UTS.ScheduleSystem/FixedConversationalRule.cs
+++ UTS.ScheduleSystem/src/UTS.ScheduleSystem/FixedConversationalRule.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace UTS.ScheduleSystem
 {
-    class FixedConversationalRule:Rule
+    public class FixedConversationalRule:Rule
     {
         public FixedConversationalRule(string id, string name, string input, string output, string relatedUsersId, Status status):base(id, name, input, output, relatedUsersId, status)
         {
