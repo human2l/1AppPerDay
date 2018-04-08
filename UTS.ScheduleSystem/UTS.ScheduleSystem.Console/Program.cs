719bbfdbbdff28dd0ad620f45ad929a4b78f45c6
Kai <human2lkai@gmail.com>
2018-04-08 18:02:31 +1000

    rebuild system with default C# class library

---
+++ UTS.ScheduleSystem/UTS.ScheduleSystem.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTS.ScheduleSystem.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Controller controller = new Controller();
+            controller.initialization();
+            System.Diagnostics.Debug.WriteLine(controller.FakeDB.UserTbl);
+            
+        }
+    }
+}
