2ba413668ae6fc01e2a295372c41b2525d251517
Yiming <543614772@qq.com>
2018-04-22 21:35:42 +1000

    Editor page update

--- UTS.ScheduleSystem/UTS.ScheduleSystem.Web/Editor_Edit.aspx
+++ UTS.ScheduleSystem/UTS.ScheduleSystem.Web/Editor_Edit.aspx
@@ -7,8 +7,35 @@
     <title></title>
 </head>
 <body>
-    <form id="form1" runat="server">
+    <form id="MainForm" runat="server">
         <div>
+            <h2>Edit rule: <asp:Label ID="ModeLabel" runat="server"/></h2>
+            <table>
+                <tr>
+                    <td>
+                        <label for="InputTextBox">Input:</label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="InputTextBox" runat="server"/>
+                        <asp:RequiredFieldValidator runat="server" ControlToValidate="InputTextBox" ErrorMessage="Input is required" Text="[Required]" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="OutputTextBox">Output:</label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="OutputTextBox" runat="server" />
+                        <asp:RequiredFieldValidator runat="server" ControlToValidate="OutputTextBox" ErrorMessage="Output is required" Text="[Required]" />
+                    </td>
+                </tr>
+                
+            </table>
+            <asp:ValidationSummary runat="server" />
+            <div>
+                <asp:Button ID="SaveButton" runat="server" OnClick="SaveButton_Click" Text="Save"/>
+                <asp:Button ID="CancelButton" runat="server" OnClick="CancelButton_Click" CausesValidation="false" Text="Cancel"/>
+            </div>
         </div>
     </form>
 </body>
--- UTS.ScheduleSystem/UTS.ScheduleSystem.Web/Editor_Edit.aspx.cs
+++ UTS.ScheduleSystem/UTS.ScheduleSystem.Web/Editor_Edit.aspx.cs
@@ -10,11 +10,71 @@ namespace UTS.ScheduleSystem.Web
     public partial class Editor_Edit : System.Web.UI.Page
     {
         public const string IdKey = "ID";
-        int? currentId;
+        string currentId;
+        Controller controller;
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["Controller"] != null)
+            {
+                controller = (Controller)Session["Controller"];
+            }
 
+            if (IsPostBack)
+            {
+                // Retrieve the ID from the view state
+
+                currentId = (string)ViewState[IdKey];
+            }
+            else
+            {
+                //Retrieve the ID from the query string
+
+                string id = Request.QueryString[IdKey];
+                if (id != null && id != "")
+                {
+                    // Edit Mode
+                    ModeLabel.Text = "Edit";
+
+                    // Save the ID for later
+                    ViewState[IdKey] = id;
+                    currentId = id;
+
+                    // Retrieve the item we're editing
+                    //var rule = (Rule)GlobalState.AddressBook.SearchById(id);
+                    //InputTextBox.Text = rule.Input;
+                    //OutputTextBox.Text = rule.Outputput;
+                }
+                else
+                {
+
+                }
+            }
+        }
+
+        protected void SaveButton_Click(object sender, EventArgs e)
+        {
+            if (currentId != null && currentId != "")
+            {
+                // Edit
+                // Find the Contact
+                // Save Changes
+                var fcRuleList = controller.FixedConversationalRulesList;
+                var cRuleList = controller.ConversationalRulesList;
+                controller.EditorService.EditPendingRule(currentId, InputTextBox.Text, OutputTextBox.Text, ref fcRuleList, ref cRuleList);
+            }
+            else
+            {
+               
+            }
+
+            // Return to the list page
+            Response.Redirect("~/Editor.aspx");
+        }
+
+        protected void CancelButton_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Editor.aspx");
         }
     }
 }
