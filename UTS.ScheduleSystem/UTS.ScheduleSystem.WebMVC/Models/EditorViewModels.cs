4a1783d39f52ce50580e5dd4c6f42d4df905b08b
Yiming <543614772@qq.com>
2018-05-20 21:18:31 +1000

    EditorMVC

---
+++ UTS.ScheduleSystem/UTS.ScheduleSystem.WebMVC/Models/EditorViewModels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using UTS.ScheduleSystem.Data;
+
+namespace UTS.ScheduleSystem.WebMVC.Models
+{
+    public class EditorViewModels
+    {
+        public List<ConversationalRule> conversionalRule = new List<ConversationalRule>();
+        public List<FixedConversationalRule> fixedConversionalRule = new List<FixedConversationalRule>();
+
+        public int Id { get; set; }
+
+        public string Input { get; set; }
+
+        public string Output { get; set; }
+
+        public string RelatedUsersId { get; set; }
+
+        public string Status { get; set; }
+
+    }
+}
