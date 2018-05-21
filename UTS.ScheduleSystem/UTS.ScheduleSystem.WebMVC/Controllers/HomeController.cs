2308192d7f4a32b6e9119606461f2aa36641f72c
Kai <human2lkai@gmail.com>
2018-05-21 16:43:52 +1000

    finished homeMVC

--- UTS.ScheduleSystem/UTS.ScheduleSystem.WebMVC/Controllers/HomeController.cs
+++ UTS.ScheduleSystem/UTS.ScheduleSystem.WebMVC/Controllers/HomeController.cs
@@ -4,19 +4,38 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using UTS.ScheduleSystem.WebMVC.Models;
+
 
 namespace UTS.ScheduleSystem.WebMVC.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index(int? input)
+        private HomeControllerViewModels _model;
+        public HomeController()
         {
-            if (input != null)
+            _model = new HomeControllerViewModels { Question = "question" };
+        }
+        public ActionResult Index()
+        {
+            ViewBag.Answer = "I'm waiting...";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(HomeControllerViewModels models)
+        {
+            if (models.Question != null)
             {
-                ViewBag.Answer = "find answer of input: " + input;
+                ViewBag.Answer = "find answer of input: " + models.Question;
+            }
+            else
+            {
+                ViewBag.Answer = "Please type something!";
             }
 
             return View();
+
         }
 
     }
---
+++ UTS.ScheduleSystem/UTS.ScheduleSystem.WebMVC/Models/HomeControllerViewModels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UTS.ScheduleSystem.WebMVC.Models
+{
+    public class HomeControllerViewModels
+    {
+        public string Question { get; set; }
+    }
+}
